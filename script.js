410a0ffdf30f5a43f94fc5e40465c33d8889d7df
EligioBiasi <biasi.eligio@edufaimm.it>
2023-05-17 15:45:17 +0200

    dice game completed

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
 // Gioco dei dadi
 // Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
-Math.floor(Math.random() * 6) + 1;
+
 document.getElementById('button').addEventListener('click',function(){
-    document.getElementById('outputhuman').innerHTML=(Math.floor(Math.random() * 6) + 1);
+   let humanNum = (parseInt(Math.floor(Math.random() * 6) + 1));
+   let humanResult = document.getElementById('outputhuman').innerHTML = 'Il tuo risultato è ='+ ' ' + humanNum
+   let computerNum = (parseInt(Math.floor(Math.random() * 6)) + 1);
+   let computerResult = document.getElementById('outputcomputer').innerHTML = 'Il risultato del computer è ='+ ' ' + computerNum
+    
+   if(humanNum < computerNum){
+        document.getElementById('final-output').innerHTML = 'Hai perso, la tua anima è mia!'
+    }else if(humanNum > computerNum){
+        document.getElementById('final-output').innerHTML = 'Hai vinto!'
+    }else{
+        document.getElementById('final-output').innerHTML = 'Pareggio!'
+    }
+
 }
 )
 // Stabilire il vincitore, in base a chi fa il punteggio più alto.
