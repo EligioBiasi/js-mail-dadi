ac53402d1b4149ec78d02a13b5f5cfa056800239
EligioBiasi <biasi.eligio@edufaimm.it>
2023-05-17 16:19:14 +0200

    added const for reference value in email

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Gioco dei dadi
+// Gioco dei dadi:
 // Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
 
 document.getElementById('button').addEventListener('click',function(){
@@ -6,15 +6,27 @@ document.getElementById('button').addEventListener('click',function(){
    let humanResult = document.getElementById('outputhuman').innerHTML = 'Il tuo risultato è ='+ ' ' + humanNum
    let computerNum = (parseInt(Math.floor(Math.random() * 6)) + 1);
    let computerResult = document.getElementById('outputcomputer').innerHTML = 'Il risultato del computer è ='+ ' ' + computerNum
-    
+
+    // Stabilire il vincitore, in base a chi fa il punteggio più alto.
+
    if(humanNum < computerNum){
         document.getElementById('final-output').innerHTML = 'Hai perso, la tua anima è mia!'
+        console.log(document.getElementById('final-output').innerHTML = 'Hai perso, la tua anima è mia!')
     }else if(humanNum > computerNum){
         document.getElementById('final-output').innerHTML = 'Hai vinto!'
+        console.log(document.getElementById('final-output').innerHTML = 'Hai vinto!')
     }else{
         document.getElementById('final-output').innerHTML = 'Pareggio!'
+        console.log(document.getElementById('final-output').innerHTML = 'Pareggio!')
     }
-
 }
 )
-// Stabilire il vincitore, in base a chi fa il punteggio più alto.
+
+// Chiedi all’utente la sua email,
+   const mailList = ["gabibbo@email.com", "riccardofalegname@email.com", "miamail.email.com"];
+    document.getElementById('button-email').addEventListener('click',function(){
+        const userEmail = document.getElementById('mail-content').value;
+        console.log(userEmail)
+    }
+    )
+//  controlla che sia nella lista di chi può accedere, stampa un messaggio appropriato sull’esito del controllo.
